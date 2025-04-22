b7ddf0a17400a09421d1610775154c04b7f1ede4
GraySky723 <2705055075@qq.com>
2025-04-22 18:17:55 +0800

    修改了输出描述

--- math_functions.py
+++ math_functions.py
@@ -3,17 +3,12 @@
 math_functions.py
 实现常用的数学运算，目前包括指数运算，后续会增加对数和开平方功能。
 """
+import math
 
 def exponent(base, power):
     """返回 base 的 power 次方"""
     return base ** power
 
-if __name__ == '__main__':
-    a, b = 2, 3
-    result = exponent(a, b)
-    print(f"{a} 的 {b} 次方: {result}")
-
-import math
 
 def logarithm(value, base):
     """
@@ -23,6 +18,7 @@ def logarithm(value, base):
     if value <= 0 or base <= 0 or base == 1:
         raise ValueError("请输入正确的正实数，并且底数不为1")
     return math.log(value, base)
+
 def square_root(value):
     """
     返回 value 的开平方，如果 value 为负数则抛出 ValueError。
@@ -30,3 +26,13 @@ def square_root(value):
     if value < 0:
         raise ValueError("负数不能开平方")
     return value ** 0.5
+
+
+if __name__ == '__main__':
+    a, b = 4, 2
+    exponent = exponent(a, b)
+    logarithm = logarithm(a, b)
+    square_root = square_root(a)
+    print(f"{a} 的 {b} 次方: {exponent}")
+    print(f"以 {b} 为底 {a} 的对数: {logarithm}")
+    print(f"{a} 的 平方根: {square_root}")
